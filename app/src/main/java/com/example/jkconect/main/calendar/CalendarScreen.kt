d0f4890d630cc589f2c60013d9e5eadad7949df9
KauaVidal <kaua.magalhaes@sptech.school>
2025-03-17 06:17:08 -0300

    tela minha agenda

--- app/src/main/java/com/example/jkconect/main/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/jkconect/main/calendar/CalendarScreen.kt
@@ -1,14 +1,19 @@
 package com.example.jkconect.main.calendar
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -19,7 +24,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
@@ -32,11 +39,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jkconect.R
+import com.example.jkconect.main.calendar.services.CardAgenda
 import com.example.jkconect.main.calendar.services.Evento
 import com.example.jkconect.ui.theme.AzulPrincipal
+import com.example.jkconect.ui.theme.RobotoCondensedFontFamily
+import com.example.jkconect.ui.theme.RobotoFontFamily
 import java.util.Locale
 
 @Composable
@@ -48,7 +64,6 @@ fun CalendarScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
     ) {
         Row(
             modifier = Modifier.padding(top = 16.dp),
@@ -135,11 +150,6 @@ fun CustomCalendar() {
             )
         }
     }
-}
-
-@Composable
-fun MinhaAgenda() {
-    Text("Minha Agenda")
 }
 
 @Composable
@@ -269,6 +279,192 @@ fun EventDayCard(
                     color = Color.White,
                     fontSize = 12.sp
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun MinhaAgenda() {
+
+    val cardsEvento = listOf(
+        CardAgenda(
+            titulo = "Evento 1",
+            confirmados = 10,
+            data = "12/03/2025"
+        ),
+        CardAgenda(
+            titulo = "Evento 2",
+            confirmados = 10,
+            data = "12/03/2025"
+        ),
+        CardAgenda(
+            titulo = "Evento 2",
+            confirmados = 10,
+            data = "12/03/2025"
+        ),
+        CardAgenda(
+            titulo = "Evento 2",
+            confirmados = 10,
+            data = "12/03/2025"
+        ),
+        CardAgenda(
+            titulo = "Evento 2",
+            confirmados = 10,
+            data = "12/03/2025"
+        ),
+    )
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+        ,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+
+        ) {
+
+        Text(
+            text = "Presenças confirmadas",
+            color = Color.White,
+            fontSize = 36.sp,
+            fontFamily = RobotoCondensedFontFamily,
+            fontWeight = FontWeight.Bold
+        )
+
+        WhiteDivider()
+
+        LazyColumn(
+            modifier = Modifier
+                .height(600.dp)
+        ) {
+
+            items(cardsEvento) { card ->
+                CardMinhaAgenda(
+                    titulo = card.titulo,
+                    confirmados = card.confirmados,
+                    data = card.data
+                )
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun WhiteDivider() {
+    HorizontalDivider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .padding(8.dp),
+        thickness = 1.dp,
+        color = Color.White
+    )
+}
+
+
+@Composable
+fun CardMinhaAgenda(
+    titulo: String,
+    confirmados: Int,
+    data: String
+) {
+    Box(modifier = Modifier.padding(24.dp)) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .width(125.dp)
+                    .height(125.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.evento_post),
+                    contentDescription = "Evento",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.clip(RoundedCornerShape(16.dp))
+                )
+            }
+
+            Box() {
+                Column(
+                    horizontalAlignment = Alignment.End,
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text(
+                        text = titulo,
+                        color = Color.White,
+                        fontSize = 18.sp,
+                        fontFamily = RobotoCondensedFontFamily,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        text = "confirmados - $confirmados",
+                        color = Color.White,
+                        fontSize = 14.sp,
+                        fontFamily = RobotoCondensedFontFamily,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Text(
+                        text = "Data: $data",
+                        color = Color.White,
+                        fontSize = 12.sp,
+                        fontFamily = RobotoCondensedFontFamily,
+                        fontWeight = FontWeight.Light
+                    )
+
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                        modifier = Modifier.padding(top = 14.dp)
+                    ) {
+                        Button(
+                            onClick = {},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = AzulPrincipal, // Cor de fundo do botão
+                                contentColor = Color.White  // Cor do
+                            ),
+                            modifier = Modifier
+                                .size(width = 60.dp, height = 20.dp)
+                                .align(Alignment.CenterVertically)
+                                .fillMaxWidth(),
+                            contentPadding = PaddingValues(0.dp)
+                        ) {
+                            Text(
+                                text = "Ver detalhes",
+                                color = Color.White,
+                                fontSize = 8.sp,
+                                fontFamily = RobotoCondensedFontFamily,
+                                fontWeight = FontWeight.Bold,
+
+                                )
+                        }
+
+                        Button(
+                            onClick = {},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Red, // Cor de fundo do botão
+                                contentColor = Color.White  // Cor do
+                            ),
+                            modifier = Modifier
+                                .size(width = 60.dp, height = 20.dp),
+                            contentPadding = PaddingValues(0.dp)
+                        ) {
+                            Text(
+                                text = "Cancelar",
+                                fontSize = 8.sp,
+                                fontFamily = RobotoCondensedFontFamily,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                    }
+
+                }
             }
         }
     }
